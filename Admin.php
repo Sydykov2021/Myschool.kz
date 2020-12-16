13cac40341be5f4d7f81cd0db4b64865095f2a31
TBcompany1 <73456171+TBcompany1@users.noreply.github.com>
2020-12-16 19:02:17 +0600

    Update Admin.php

--- Admin.php
+++ Admin.php
@@ -1,1 +1,17 @@
-
+<? include("includes/db.php");
+$echo = "Тут скоро что-то будет";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+             <title>Админка</title>
+             <link rel="stylesheet" href="admin.css">
+</head>
+<body>
+             <div class='wrapper'>
+                           <main class='main' id='main'>
+                                          <?echo $echo;?>
+                          </main>
+             </div>
+</body>
+</html>
